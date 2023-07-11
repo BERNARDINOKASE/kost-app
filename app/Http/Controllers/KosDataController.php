240f6e4a416b51907d6129a81d4d2c7844037fc8
Bernardino Antonius Kase <reynaldokase.rk@gmail.com>
2023-07-11 12:20:40 +0800

    setup fix admin panel with views

--- app/Http/Controllers/KosDataController.php
+++ app/Http/Controllers/KosDataController.php
@@ -7,6 +7,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\File;
 
 
+
 class KosDataController extends Controller
 {
     /**
--- resources/views/admin/layout/master.blade.php
+++ resources/views/admin/layout/master.blade.php
@@ -62,7 +62,7 @@
     </div>
     <script src="{{asset('assets/')}}/js/bootstrap.js"></script>
     <script src="{{asset('assets/')}}/js/app.js"></script>
-    {{-- @include('sweetalert::alert') --}}
+    @include('sweetalert::alert')
 
     @yield('scripts')
     
